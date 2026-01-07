3c4942c143fc410da006b657e60e35db525f74ff
Alka kumari <alkamishra1960@gmail.com>
2026-01-07 10:01:10 +0530

    db-config change

--- src/db.js
+++ src/db.js
@@ -2,8 +2,10 @@
 const { Pool } = require('pg');
 
 const db = new Pool({
-    connectionString: process.env.POSTGRES_URL,
-    
+    host : process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    database : process.env.POSTGRES_DATABASE,
+    password : process.env.POSTGRES_PASSWORD
 })
 
 
